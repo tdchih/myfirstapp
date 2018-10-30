0d7833a358d4ef40b28f1a34c7b040929916ee19
Chih Ted <tedchihhaha@gmail.com>
2018-10-30 19:34:57 +0800

    Initial Commit

---
+++ myfirstapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myfirstapp
+//
+//  Created by Chih Ted on 2018/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
